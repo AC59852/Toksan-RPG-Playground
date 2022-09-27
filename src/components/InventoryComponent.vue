f583a5b4d177ba9e2a4b44d3540d7f3faeff441b
AC59852 <austin.caron1@gmail.com>
2022-09-27 03:00:11 -0400

    added currency validation. attempted cleaning up of some more reads, adding the items on mount of app instead of shop component

--- src/components/InventoryComponent.vue
+++ src/components/InventoryComponent.vue
@@ -1,6 +1,7 @@
 <template>
   <section id="inventory">
     <h2>Inventory</h2>
+    <h2>Money: {{currency}}</h2>
     <div class="inventory__container">
       <div v-for="item in inventory" :key="item.id">{{item}}</div>
     </div>
@@ -8,12 +9,10 @@
 </template>
 
 <script>
-
+import { db } from '@/db'
 export default {
   name: 'InventoryComponent',
-
   props: ['user'],
-
   data() {
     return {
       inventory: [],
@@ -21,11 +20,34 @@ export default {
     }
   },
 
-  mounted() {
+  watch: {
+    user() {
+      console.log("test")
+      // set the inventory
+      this.playerInventory = this.user
+      // query the items collection
+      db.collection('items')
+      .get()
+      .then(querySnapshot => {
+        const items = querySnapshot.docs.map(doc => doc.data())
+        // loop through the inventory
+        this.playerInventory.forEach(item => {
+          // find the item in the items collection based on the string provided
+          const foundItem = items.find(i => i.itemID === item)
+          // add the item to the inventory array
+          this.inventory.push(foundItem)
+          // update the vuex inventory state
+          this.$store.commit('updateInventory', this.inventory)
+          console.log(this.$store.state.inventory)
+        })
+      })
+    }
   },
 
-  watch: {
-    
+  computed: {
+    currency() {
+      return this.$store.state.user.playerCurrency
+    }
   },
 
   methods: {
@@ -34,5 +56,4 @@ export default {
 </script>
 
 <style>
-
 </style>
--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -2,7 +2,7 @@
   <section id="shop">
     <h2>Shop</h2>
     <div class="shop__container">
-      <div @click="purchase(item)" v-for="item in items" :key="item.itemID">
+      <div @click="purchase(item, $event)" v-for="item in items" :key="item.itemID">
         {{item.name}}
       </div>
     </div>
@@ -10,32 +10,31 @@
 </template>
 
 <script>
-import { db } from '@/db'
 export default {
-  data() {
-    return {
-      items: []
-    }
-  },
-
-  mounted() {
-    // get the items collection
-    db.collection('items')
-    .get()
-    .then(querySnapshot => {
-      const items = querySnapshot.docs.map(doc => doc.data())
-
-      // set the items array
-      this.items = items
-
-      console.log(items)
-    })
+  computed: {
+    items() {
+      return this.$store.state.items
+    },
   },
 
   methods: {
-    purchase(item) {
-      // add the item to the user's inventory
-      this.$store.commit('addToInventory', item)
+    purchase(item, event) {
+
+      let cost = item.cost
+
+      if(this.$store.state.user.playerCurrency >= cost) {
+        // subtract the cost from the user's currency
+        this.$store.commit('subtractCurrency', cost)
+        
+        // add the item to the user's inventory
+        this.$store.commit('addToInventory', item)
+
+        // remove the element from the DOM
+        event.target.remove()
+      } else {
+        alert("You don't have enough money!")
+      }
+      
     }
   }
 }
